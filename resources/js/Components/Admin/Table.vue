756aa1c9f4adfd0ede505b4ee17179b6023c7565
idleo <idleo@ya.ru>
2022-03-01 11:30:49 +0300

    Restyle table view for admin pages, make responsive

--- resources/js/Components/Admin/Table.vue
+++ resources/js/Components/Admin/Table.vue
@@ -10,13 +10,14 @@ defineProps({
 </script>
 
 <template>
-  <table class="items-center bg-transparent border-collapse border-b-2 border-b-gray-100">
-    <thead>
-      <tr>
+  <table class="flex lg:table items-center bg-transparent border-collapse border-b-2 border-b-gray-100">
+    <thead class="hidden lg:table-header-group">
+      <tr class="table-row">
         <th
           v-for="(head, k) in headers"
           :key="k"
           class="
+            table-cell
             px-6
             bg-blueGray-50
             text-blueGray-500
@@ -37,28 +38,33 @@ defineProps({
         <th v-if="actions?.length !== 0" style="width: 1%" />
       </tr>
     </thead>
-    <tbody>
+    <tbody class="block w-full lg:table-row-group">
       <tr
         v-for="(row, id) in rows"
         :key="id"
-        class="hover:bg-blue-200 duration-200"
+        class="block shadow-xl mb-6 p-4 lg:p-0 border-2 border-gray-200 lg:border-0 lg:mb-0 lg:shadow-none lg:table-row hover:bg-blue-200 duration-200"
         :class="{ 'bg-gray-100': id % 2 === 0 }"
       >
         <td
           v-for="(header, hid) in headers"
           :key="row.id + hid"
           class="
+            block
+            lg:table-cell
             border-t-0
             px-6
             align-middle
             border-l-0 border-r-0
             text-sm
-            whitespace-nowrap
+            whitespace-break
             p-2
             text-left
           "
         >
-          {{ row[header.name] }}
+          <span class="inline-block lg:hidden text-gray-600 underline">{{ header.name }}:</span>
+          <span class="break-words">
+            {{ row[header.name] }}
+          </span>
         </td>
       </tr>
     </tbody>
